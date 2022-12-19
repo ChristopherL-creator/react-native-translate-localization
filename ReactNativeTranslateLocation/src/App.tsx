da0a45e14444c9b33ee266cff80c63fcaa5c01d0
Christopher Limone <imsteveim14@gmail.com>
2022-12-19 11:53:57 +0100

    first commit

---
+++ ReactNativeTranslateLocation/src/App.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <View> 
+      <Text> ciao, questo testo è di prova =D </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
